0e24df4f47442d435f8dd3db04bf6c6b34f58087
FraSharp <f.duca00@gmail.com>
2024-09-26 22:08:42 +0200

    pwdgen: update header accordingly
    Signed-off-by: FraSharp <f.duca00@gmail.com>

--- pwdgen.h
+++ pwdgen.h
@@ -1,8 +1,12 @@
 #include <stdint.h>
 
-extern const char chars[];
+extern const char lowercase[];
+extern const char uppercase[];
+extern const char digits[];
+extern const char specials[];
 
 int pwd_len(uint_fast8_t len);
+uint_fast8_t rrand(uint_fast8_t bound);
 char *pwdgen(uint_fast8_t len);
 int main(int argc, char *argv[]);
 
